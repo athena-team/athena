92ca986f52cd4b1ec948dbae232abfe29befba9a
chenguoguo <chenguoguo06@gmail.com>
2020-01-03 06:47:46 +0000

    Several updates:1. examples/asr/hkust/local/prepare_data.py now takes an input and output directory, this way we avoid modifying the dataset directory as we don't want to mess up with the original data (and it's usually read-only), and the output directory also acts as the working directory; 2. use the sph2pipe under tools/sph2pipe/sph2pipe; 3. added exit to run.sh so that we can properly catch errors from pipe

--- examples/asr/hkust/run.sh
+++ examples/asr/hkust/run.sh
@@ -24,49 +24,54 @@ source tools/env.sh
 
 stage=0
 stop_stage=100
+dataset_dir=/nfs/project/datasets/opensource_data/hkust
 
 if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then
     # prepare data
     echo "Preparing data"
-    python examples/asr/hkust/local/prepare_data.py /nfs/project/datasets/opensource_data/hkust
-    mkdir -p examples/asr/hkust/data
-    cp /nfs/project/datasets/opensource_data/hkust/{train,dev}.csv examples/asr/hkust/data/
+    mkdir -p examples/asr/hkust/data || exit 1
+    python examples/asr/hkust/local/prepare_data.py \
+        $dataset_dir examples/asr/hkust/data || exit 1
 
-    # cal cmvn
+    # calculate cmvn
     cat examples/asr/hkust/data/train.csv > examples/asr/hkust/data/all.csv
     tail -n +2 examples/asr/hkust/data/dev.csv >> examples/asr/hkust/data/all.csv
-    tail -n +2 examples/asr/hkust/data/test.csv >> examples/asr/hkust/data/all.csv
-    python athena/cmvn_main.py examples/asr/hkust/mpc.json examples/asr/hkust/data/all.csv
+    python athena/cmvn_main.py \
+      examples/asr/hkust/mpc.json examples/asr/hkust/data/all.csv || exit 1
 fi
 
 if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
     # pretrain stage
     echo "Pretraining"
-    python athena/main.py examples/asr/hkust/mpc.json
+    python athena/main.py examples/asr/hkust/mpc.json || exit 1
 fi
 
 if [ ${stage} -le 2 ] && [ ${stop_stage} -ge 2 ]; then
     # finetuning stage
     echo "Fine-tuning"
-    python athena/main.py examples/asr/hkust/mtl_transformer.json
+    python athena/main.py examples/asr/hkust/mtl_transformer.json || exit 1
 fi
 
 if [ ${stage} -le 3 ] && [ ${stop_stage} -ge 3 ]; then
     # decoding stage
     echo "Decoding"
     # prepare language model
-    tail -n +2 examples/asr/hkust/data/train.csv | cut -f 3 > examples/asr/hkust/data/text
-    python examples/asr/hkust/local/segment_word.py examples/asr/hkust/data/vocab \
-       examples/asr/hkust/data/text > examples/asr/hkust/data/text.seg
-    tools/kenlm/build/bin/lmplz -o 4 < examples/asr/hkust/data/text.seg > examples/asr/hkust/data/4gram.arpa
+    tail -n +2 examples/asr/hkust/data/train.csv |\
+        cut -f 3 > examples/asr/hkust/data/text || exit 1
+    python examples/asr/hkust/local/segment_word.py \
+        examples/asr/hkust/data/vocab \
+        examples/asr/hkust/data/text \
+        > examples/asr/hkust/data/text.seg || exit 1
+    tools/kenlm/build/bin/lmplz -o 4 < examples/asr/hkust/data/text.seg \
+        > examples/asr/hkust/data/4gram.arpa || exit 1
 
-    python athena/decode_main.py examples/asr/hkust/mtl_transformer.json
+    python athena/decode_main.py examples/asr/hkust/mtl_transformer.json || exit 1
 fi
 
 if [ ${stage} -le 4 ] && [ ${stop_stage} -ge 4 ]; then
     echo "training rnnlm"
     tail -n +2 examples/asr/hkust/data/train.csv | awk '{print $3"\t"$3}' > examples/asr/hkust/data/train.trans.csv
     tail -n +2 examples/asr/hkust/data/dev.csv | awk '{print $3"\t"$3}' > examples/asr/hkust/data/dev.trans.csv
-    python athena/main.py examples/asr/hkust/rnnlm.json
+    python athena/main.py examples/asr/hkust/rnnlm.json || exit 1
 fi
 
