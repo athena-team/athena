4e92a8d10cc32ce6f5af556672776a0bf93382c0
chenguoguo <chenguoguo06@gmail.com>
2020-01-03 06:41:18 +0000

    Updated sph2pipe.sh so that sph2pipe sits in tools/sph2pipe

--- tools/install_sph2pipe.sh
+++ tools/install_sph2pipe.sh
@@ -5,6 +5,8 @@ if [ ! -e sph2pipe_v2.5.tar.gz ]; then
   wget -T 10 -t 3 http://www.openslr.org/resources/3/sph2pipe_v2.5.tar.gz || exit 1
 fi
 
+rm -rf sph2pipe_v2.5 sph2pipe
 tar -zxvf sph2pipe_v2.5.tar.gz || exit 1
-cd sph2pipe_v2.5/
+mv sph2pipe_v2.5 sph2pipe
+cd sph2pipe
 gcc -o sph2pipe  *.c -lm || exit 1
