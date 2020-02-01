b161e071f84ebd5f723160e68a82726f67af1ea4
Guoguo Chen <chenguoguo06@gmail.com>
2020-01-31 21:38:34 -0800

    Cleaning up aishell recipe (#46)
    * Moving json files to configs for aishell recipe
    
    * For aishell, writing csv files to the output working directory directly

--- examples/asr/aishell/run.sh
+++ examples/asr/aishell/run.sh
@@ -20,51 +20,66 @@ if [ "athena" != $(basename "$PWD") ]; then
     exit 1
 fi
 
-stage=1
+source tools/env.sh
+
+stage=0
 stop_stage=100
+horovod_cmd="horovodrun -np 4 -H localhost:4"
+horovod_prefix="horovod_"
+dataset_dir=/nfs/cold_project/datasets/opensource_data/aishell/data_aishell
 
 if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then
     # prepare data
-    echo "making csv"
+    echo "Creating csv"
     mkdir -p examples/asr/aishell/data
-    python examples/asr/aishell/local/prepare_data.py /nfs/cold_project/datasets/opensource_data/aishell/data_aishell
-    cp /nfs/cold_project/datasets/opensource_data/aishell/data_aishell/{train,dev,test}.csv examples/asr/aishell/data/
+    python examples/asr/aishell/local/prepare_data.py \
+        $dataset_dir examples/asr/aishell/data || exit 1
 fi
 
 if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
-    # cal cmvn
-    echo "computing cmvn"
+    # calculate cmvn
+    echo "Computing cmvn"
     cat examples/asr/aishell/data/train.csv > examples/asr/aishell/data/all.csv
     tail -n +2 examples/asr/aishell/data/dev.csv >> examples/asr/aishell/data/all.csv
     tail -n +2 examples/asr/aishell/data/test.csv >> examples/asr/aishell/data/all.csv
-    python athena/cmvn_main.py examples/asr/aishell/mpc.json examples/asr/aishell/data/all.csv
+    python athena/cmvn_main.py \
+        examples/asr/aishell/configs/mpc.json examples/asr/aishell/data/all.csv || exit 1
 fi
 
 if [ ${stage} -le 2 ] && [ ${stop_stage} -ge 2 ]; then
     # pretrain stage
     echo "Pretraining"
-    python athena/main.py examples/asr/aishell/mpc.json
+    $horovod_cmd python athena/${horovod_prefix}main.py \
+        examples/asr/aishell/configs/mpc.json || exit 1
 fi
 
 if [ ${stage} -le 3 ] && [ ${stop_stage} -ge 3 ]; then
     # finetuning stage
     echo "Fine-tuning"
-    python athena/main.py examples/asr/aishell/mtl_transformer_sp.json
+    $horovod_cmd python athena/${horovod_prefix}main.py \
+        examples/asr/aishell/configs/mtl_transformer_sp.json || exit 1
 fi
 
 if [ ${stage} -le 4 ] && [ ${stop_stage} -ge 4 ]; then
     # prepare language model
-    echo "training language model ..."
-    tail -n +2 examples/asr/aishell/data/train.csv | cut -f 3 > examples/asr/aishell/data/text
-    # tools/kenlm/build/bin/lmplz -o 4 < examples/asr/aishell/data/text > examples/asr/aisehll/data/4gram.arpa
-    tail -n +2 examples/asr/aishell/data/train.csv | awk '{print $3"\t"$3}' > examples/asr/aishell/data/train.trans.csv
-    tail -n +2 examples/asr/aishell/data/dev.csv | awk '{print $3"\t"$3}' > examples/asr/aishell/data/dev.trans.csv
-    tail -n +2 examples/asr/aishell/data/test.csv | awk '{print $3"\t"$3}' > examples/asr/aishell/data/test.trans.csv
-    python athena/main.py examples/asr/aishell/rnnlm.json
+    echo "Training language model ..."
+    tail -n +2 examples/asr/aishell/data/train.csv |\
+        cut -f 3 > examples/asr/aishell/data/text
+    # tools/kenlm/build/bin/lmplz -o 4 < examples/asr/aishell/data/text \
+    #     > examples/asr/aisehll/data/4gram.arpa || exit 1
+    tail -n +2 examples/asr/aishell/data/train.csv |\
+        awk '{print $3"\t"$3}' > examples/asr/aishell/data/train.trans.csv
+    tail -n +2 examples/asr/aishell/data/dev.csv |\
+        awk '{print $3"\t"$3}' > examples/asr/aishell/data/dev.trans.csv
+    tail -n +2 examples/asr/aishell/data/test.csv |\
+        awk '{print $3"\t"$3}' > examples/asr/aishell/data/test.trans.csv
+    $horovod_cmd python athena/${horovod_prefix}main.py \
+        examples/asr/aishell/configs/rnnlm.json || exit 1
 fi
 
 if [ ${stage} -le 5 ] && [ ${stop_stage} -ge 5 ]; then
     # decoding stage
-    echo "running decode ..."
-    python athena/decode_main.py examples/asr/aishell/mtl_transformer_sp.json
+    echo "Running decode ..."
+    python athena/decode_main.py \
+        examples/asr/aishell/configs/mtl_transformer_sp.json || exit 1
 fi
