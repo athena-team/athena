8feede0dbcf5098f0cd72611ed95d1a461c44cc4
Dongwei Jiang <jiangdongwei@didichuxing.com>
2020-04-01 14:56:26 +0800

    use import checks, single-gpu mode can still be used without installing Horovod

--- athena/__init__.py
+++ athena/__init__.py
@@ -50,6 +50,7 @@ from .models.translate_transformer import NeuralTranslateTransformer
 
 # solver & loss & accuracy
 from .solver import BaseSolver
+from .solver import HorovodSolver
 from .solver import DecoderSolver
 from .loss import CTCLoss
 from .loss import Seq2SeqSparseCategoricalCrossentropy
