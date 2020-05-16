a942297cb73768f8edafa5d255f92ee07a709c05
cookingbear <zhangruixiong@didiglobal.com>
2020-05-16 16:09:12 +0800

    specaugment

--- athena/data/datasets/preprocess.py
+++ athena/data/datasets/preprocess.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-# Copyright (C) ATHENA AUTHORS
+# Copyright (C) ATHENA AUTHORS; Ruixiong Zhang;
 # All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
