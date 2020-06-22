98e66b480947bbbb7de927500c1be97b2f174bc8
xiaochunxin <470484768@qq.com>
2020-06-22 11:27:57 +0800

    run

--- examples/vc/vcc2020/run.sh
+++ examples/vc/vcc2020/run.sh
@@ -21,7 +21,7 @@ fi
 
 source tools/env.sh
 
-stage=1
+stage=0
 stop_stage=100
 horovod_cmd="horovodrun -np 4 -H localhost:4"
 horovod_prefix="horovod_"
@@ -43,45 +43,21 @@ if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
     tail -n +2 examples/vc/vcc2020/data/test.csv >> examples/vc/vcc2020/data/all.csv
     # python CUDA_VISIBLE_DEVICES='' athena/cmvn_main.py 
 	python athena/cmvn_main.py \
-        examples/vc/vcc2020/configs/VC.json examples/vc/vcc2020/data/all.csv || exit 1
+        examples/vc/vcc2020/configs/stargan_voice_conversion.json examples/vc/vcc2020/data/all.csv || exit 1
 fi
 
-if [ ${stage} -le 2 ] && [ ${stop_stage} -ge 2 ]; then
-    # pretrain stage
-    echo "Pretraining"
-    # $horovod_cmd python athena/${horovod_prefix}main.py \
-        # examples/vc/vcc2020/configs/mpc.json || exit 1
-	python athena/main.py \
-		examples/vc/vcc2020/configs/mpc.json || exit 1
-fi
 
-if [ ${stage} -le 3 ] && [ ${stop_stage} -ge 3 ]; then
-    # finetuning stage
-    echo "Fine-tuning"
+if [ ${stage} -le 2 ] && [ ${stop_stage} -ge 3 ]; then
+    # training stage
+    echo "training stargan"
     $horovod_cmd python athena/${horovod_prefix}main.py \
-        examples/vc/vcc2020/configs/mtl_transformer_sp.json || exit 1
+        examples/vc/vcc2020/configs/stargan_voice_conversion.json || exit 1
 fi
 
-if [ ${stage} -le 4 ] && [ ${stop_stage} -ge 4 ]; then
-    # prepare language model
-    echo "Training language model ..."
-    tail -n +2 examples/vc/vcc2020/data/train.csv |\
-        cut -f 3 > examples/vc/vcc2020/data/text
-    # tools/kenlm/build/bin/lmplz -o 4 < examples/vc/vcc2020/data/text \
-    #     > examples/vc/aisehll/data/4gram.arpa || exit 1
-    tail -n +2 examples/vc/vcc2020/data/train.csv |\
-        awk '{print $3"\t"$3}' > examples/vc/vcc2020/data/train.trans.csv
-    tail -n +2 examples/vc/vcc2020/data/dev.csv |\
-        awk '{print $3"\t"$3}' > examples/vc/vcc2020/data/dev.trans.csv
-    tail -n +2 examples/vc/vcc2020/data/test.csv |\
-        awk '{print $3"\t"$3}' > examples/vc/vcc2020/data/test.trans.csv
-    $horovod_cmd python athena/${horovod_prefix}main.py \
-        examples/vc/vcc2020/configs/rnnlm.json || exit 1
-fi
 
-if [ ${stage} -le 5 ] && [ ${stop_stage} -ge 5 ]; then
-    # decoding stage
-    echo "Running decode ..."
-    python athena/decode_main.py \
-        examples/vc/vcc2020/configs/mtl_transformer_sp.json || exit 1
+if [ ${stage} -le 3 ] && [ ${stop_stage} -ge 5 ]; then
+    # convert stage
+    echo "convert wavs ..."
+    python athena/convert_wavs.py \
+        examples/vc/vcc2020/configs/stargan_voice_conversion.json || exit 1
 fi
