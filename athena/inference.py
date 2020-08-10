991c44fd822dab023167f3926dbadf00d090d74d
cookingbear <664610407@qq.com>
2020-08-10 14:28:38 +0800

    merge decode main

--- athena/inference.py
+++ athena/inference.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-# Copyright (C) ATHENA AUTHORS
+# Copyright (C) ATHENA AUTHORS; Ruixiong Zhang
 # All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -16,35 +16,43 @@
 # ==============================================================================
 # Only support tensorflow 2.0
 # pylint: disable=invalid-name, no-member, redefined-outer-name
-r""" an implementation of decoding for speech recognition models """
+r""" entry point for inference of all kinds of models """
 import sys
 import json
 import tensorflow as tf
 from absl import logging
-from athena import DecoderSolver
 from athena.main import (
     parse_config,
     build_model_from_jsonfile,
     SUPPORTED_DATASET_BUILDER
 )
+from athena import BaseSolver, DecoderSolver, SynthesisSolver, HorovodSolver
 
+try:
+    import horovod.tensorflow as hvd
+except ImportError:
+    print("There is some problem with your horovod installation. \
+           But it wouldn't affect single-gpu inference")
 
-def decode(jsonfile, rank_size=1, rank=0):
-    """ entry point for model decoding, do some preparation work """
+SOLVERS = {
+    "asr": DecoderSolver,
+    "tts": SynthesisSolver
+}
+
+def inference(jsonfile, rank_size=1, rank=0):
+    """ entry point for model inference, do some preparation work """
     p, model, _, checkpointer = build_model_from_jsonfile(jsonfile)
     avg_num = 1 if 'model_avg_num' not in p.decode_config else p.decode_config['model_avg_num']
-    checkpointer.compute_nbest_avg(avg_num)
-    lm_model = None
-    if 'lm_type' in p.decode_config and p.decode_config['lm_type'] == "rnn":
-        _, lm_model, _, lm_checkpointer = build_model_from_jsonfile(p.decode_config['lm_path'])
-        lm_checkpointer.restore_from_best()
-
-    solver = DecoderSolver(model, config=p.decode_config, lm_model=lm_model)
+    if avg_num > 0:
+        checkpointer.compute_nbest_avg(avg_num)
     assert p.testset_config is not None
     dataset_builder = SUPPORTED_DATASET_BUILDER[p.dataset_builder](p.testset_config)
     dataset_builder.shard(rank_size, rank)
     logging.info("shard result: %d" % len(dataset_builder))
-    solver.decode(dataset_builder.as_dataset(batch_size=1), rank_size=rank_size)
+
+    inference_solver = SOLVERS[p.solver_type]
+    solver = inference_solver(model, dataset_builder, config=p.decode_config)
+    solver.inference(dataset_builder.as_dataset(batch_size=1), rank_size=rank_size)
 
 
 if __name__ == "__main__":
@@ -59,5 +67,18 @@ if __name__ == "__main__":
         config = json.load(file)
     p = parse_config(config)
 
-    DecoderSolver.initialize_devices(p.solver_gpu)
-    decode(jsonfile)
+    rank_size = 1
+    rank_index = 0
+    try:
+        hvd.init()
+        rank_size = hvd.size()
+        rank_index = hvd.rank()
+    except ImportError:
+        print("There is some problem with your horovod installation. \
+               But it wouldn't affect single-gpu inference")
+    if rank_size > 1:
+        HorovodSolver.initialize_devices(p.solver_gpu)
+    else:
+        BaseSolver.initialize_devices(p.solver_gpu)
+
+    inference(jsonfile, rank_size=rank_size, rank=rank_index)
