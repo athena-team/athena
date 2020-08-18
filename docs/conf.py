9b2bfe93e317480c1302739f4b3eff87d60b52ee
neneluo <nene.luo@gmail.com>
2020-08-18 16:07:00 +0800

    change docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -36,6 +36,11 @@ extensions = [
     'sphinx.ext.napoleon',
 ]
 
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown',
+}
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
